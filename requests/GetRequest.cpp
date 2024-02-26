34dda0d2ac60c17321f8f9269f0084db76fe4c6d
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-02-26 21:01:52 +0100

    m

--- requests/GetRequest.cpp
+++ requests/GetRequest.cpp
@@ -15,8 +15,15 @@ std::string GetRequest::handle() {
     //if (Location* loc = dynamic_cast<Index*>(conf->server(0).bestLocation(_uri)))
 	//	return "";
 
-	if (Location* loc = dynamic_cast<Index*>(getServerInst().bestLocation(_uri)))
-		return "";
+	if (Index* loc = dynamic_cast<Index*>(getServerInst().bestLocation(_uri)))
+	{
+		std::cout << "GetRequest::handle() location found: _uri [" << _uri << "]" << std::endl;
+		std::cout << "GetRequest::handle() location found: loc->index() [" << loc->index() << "]" << std::endl;
+		std::cout << "GetRequest::handle() location found: loc->autoindex() [" << loc->autoindex() << "]" << std::endl;
+		std::cout << "GetRequest::handle() location found: loc->cgi() [" << loc->cgi() << "]" << std::endl;
+		std::cout << "GetRequest::handle() location found: loc->file() [" << loc->file() << "]\n" << std::endl;
+		//return "";
+	}
 
 	Logger::info("GetRequest::handle() handling GET request");
 
