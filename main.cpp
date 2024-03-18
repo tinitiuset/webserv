56c652e4d782f341343d07365b34b468a000b533
tinitiuset <tinitiuset@gmail.com>
2024-03-18 18:05:25 +0100

    Refactor and add better exception handling

--- main.cpp
+++ main.cpp
@@ -9,13 +9,20 @@ int main(int argc, char **argv)
 	if (argc == 1) conf = new Conf("./config/default.conf");
   	else if (argc == 2) conf = new Conf(argv[1]);
   	else return std::cerr << "Usage: " << argv[0] << " [config_file]" << std::endl, 1;
-	
-	if (!conf->parse()) return (delete conf, 1);
 
-	conf->load();
+	try {
+		conf->parse();
+		conf->load();
+		Multiplexer().run();
+	}
+	catch (std::runtime_error &e) {
+		Logger::error(e.what());
+		return (delete conf, 1);
+	}
+	catch (std::exception &e) {
+		Logger::error(e.what());
+		return (delete conf, 1);
+	}
 
-	Multiplexer().run();
-
-	delete conf;
-	return 0;
+	return (delete conf, 0);
 }
--- model/Server.cpp
+++ model/Server.cpp
@@ -1,15 +1,13 @@
 #include "Server.hpp"
-#include "arpa/inet.h"
-#include "../exceptions/RequestException.hpp"
 
-void sanitize(std::string& serverBlock) {
+void sanitize(std::string&serverBlock) {
 	std::string result;
 	char prevChar = 0;
 	for (size_t i = 0; i < serverBlock.size(); ++i) {
-		
 		if (serverBlock[i] == ' ' && prevChar == '\n') {
 			continue;
-		} else {
+		}
+		else {
 			result += serverBlock[i];
 		}
 		prevChar = serverBlock[i];
@@ -19,7 +17,7 @@ void sanitize(std::string& serverBlock) {
 	serverBlock.erase(std::remove(serverBlock.begin(), serverBlock.end(), '\n'), serverBlock.end());
 }
 
-Server::Server(std::string& serverBlock): _fd(-1) {
+Server::Server(std::string&serverBlock): _fd(-1), _port(-1), _body_size(0) {
 	sanitize(serverBlock);
 
 	size_t pos = 0;
@@ -32,10 +30,9 @@ Server::Server(std::string& serverBlock): _fd(-1) {
 		std::string locationBlock = serverBlock.substr(pos, endPos - pos + 1);
 
 		Location* location;
-		(locationBlock.find("redirect") != std::string::npos) ?
-			location = new Redirect(locationBlock)
-		:
-			location = new Index(locationBlock);
+		(locationBlock.find("redirect") != std::string::npos)
+			? location = new Redirect(locationBlock)
+			: location = new Index(locationBlock);
 		_locations.push_back(location);
 
 		pos = endPos + 1;
@@ -44,7 +41,8 @@ Server::Server(std::string& serverBlock): _fd(-1) {
 	pos = serverBlock.find("listen");
 	if (pos != std::string::npos) {
 		_address = serverBlock.substr(pos + 7, serverBlock.find(':', pos) - pos - 7);
-		_port = Utils::toInt(serverBlock.substr(serverBlock.find(':', pos) + 1, serverBlock.find(';', pos) - serverBlock.find(':', pos) - 1));
+		_port = Utils::toInt(serverBlock.substr(serverBlock.find(':', pos) + 1,
+		                     serverBlock.find(';', pos) - serverBlock.find(':', pos) - 1));
 	}
 
 	pos = serverBlock.find("server_name");
@@ -66,25 +64,27 @@ Server::Server(std::string& serverBlock): _fd(-1) {
 	pos = 0;
 	while ((pos = serverBlock.find("error_page", pos)) != std::string::npos) {
 		int code = Utils::toInt(serverBlock.substr(pos + 11, serverBlock.find(' ', pos + 11) - pos - 11));
-		std::string path = serverBlock.substr(serverBlock.find(' ', pos + 11) + 1, serverBlock.find(';', pos) - serverBlock.find(' ', pos + 11) - 1);
+		std::string path = serverBlock.substr(serverBlock.find(' ', pos + 11) + 1,
+		                                      serverBlock.find(';', pos) - serverBlock.find(' ', pos + 11) - 1);
 		_error_pages[code] = path;
 		pos = serverBlock.find(';', pos);
 	}
 }
 
-Server::Server(const Server& other): _server_name(other._server_name), _address(other._address), _fd(other._fd), _port(other._port),
-  _body_size(other._body_size), _root(other._root)
-{
-	for (std::list<Location*>::const_iterator it = other._locations.begin(); it != other._locations.end(); ++it) {
+Server::Server(const Server&other): _server_name(other._server_name), _address(other._address), _fd(other._fd),
+                                    _port(other._port),
+                                    _body_size(other._body_size), _root(other._root) {
+	for (std::list<Location *>::const_iterator it = other._locations.begin(); it != other._locations.end(); ++it) {
 		_locations.push_back((*it)->clone());
 	}
-	for (std::map<int, std::string>::const_iterator it = other._error_pages.begin(); it != other._error_pages.end(); ++it) {
+	for (std::map<int, std::string>::const_iterator it = other._error_pages.begin(); it != other._error_pages.end(); ++
+	     it) {
 		_error_pages[it->first] = it->second;
 	}
 }
 
 Server::~Server() {
-	for (std::list<Location*>::iterator it = _locations.begin(); it != _locations.end(); ++it)
+	for (std::list<Location *>::iterator it = _locations.begin(); it != _locations.end(); ++it)
 		delete *it;
 }
 
@@ -96,12 +96,12 @@ int Server::fd() const { return _fd; }
 
 int Server::port() const { return _port; }
 
-int Server::body_size() const {	return _body_size; }
+int Server::body_size() const { return _body_size; }
 
 std::string Server::root() const { return _root; }
 
-Location* Server::location(const std::string& path) const {
-	for (std::list<Location*>::const_iterator it = _locations.begin(); it != _locations.end(); ++it) {
+Location* Server::location(const std::string&path) const {
+	for (std::list<Location *>::const_iterator it = _locations.begin(); it != _locations.end(); ++it) {
 		Location* location = *it;
 		if (path.find(location->path()) == 0)
 			return location;
@@ -110,11 +110,11 @@ Location* Server::location(const std::string& path) const {
 	return NULL;
 }
 
-Location* Server::bestLocation(const std::string& path) const {
+Location* Server::bestLocation(const std::string&path) const {
 	std::size_t longestMatch = 0;
 	Location* bestLocation = NULL;
 
-	for (std::list<Location*>::const_iterator it = _locations.begin(); it != _locations.end(); ++it) {
+	for (std::list<Location *>::const_iterator it = _locations.begin(); it != _locations.end(); ++it) {
 		Location* location = *it;
 		if (location->path().length() > longestMatch) {
 			if (path.find(location->path()) == 0) {
@@ -126,36 +126,34 @@ Location* Server::bestLocation(const std::string& path) const {
 	return bestLocation;
 }
 
-in_addr_t	Server::custom_inet_addr(const std::string &ip_str)
-{
-    std::stringstream ss(ip_str);
+in_addr_t Server::custom_inet_addr(const std::string&ip_str) {
+	std::stringstream ss(ip_str);
 
-    int octets[4];
-    char dot;
+	int octets[4];
+	char dot;
 
-    for (int i = 0; i < 4; ++i)
-    {
-        ss >> octets[i];
-        if (i < 3) {
-            ss >> dot;
-            if (dot != '.') {
-                std::cerr << "Error: Separator not '.'\n";
-                return 0;
-            }
-        }
-    }
+	for (int i = 0; i < 4; ++i) {
+		ss >> octets[i];
+		if (i < 3) {
+			ss >> dot;
+			if (dot != '.') {
+				std::cerr << "Error: Separator not '.'\n";
+				return 0;
+			}
+		}
+	}
 
-    in_addr_t ip_address = 0;
-    for (int j = 0; j < 4; ++j) {
-        if (octets[j] < 0 || octets[j] > 255) {
-            std::cerr << "Error: Invalid octet values\n";
-            return 0;
-        }
-        ip_address |= (static_cast<in_addr_t>(octets[j]) << (8 * (3 - j)));
-    }
+	in_addr_t ip_address = 0;
+	for (int j = 0; j < 4; ++j) {
+		if (octets[j] < 0 || octets[j] > 255) {
+			std::cerr << "Error: Invalid octet values\n";
+			return 0;
+		}
+		ip_address |= (static_cast<in_addr_t>(octets[j]) << (8 * (3 - j)));
+	}
 
-    return ip_address;
-} 
+	return ip_address;
+}
 
 
 std::string Server::errorPage(int code) const {
@@ -166,26 +164,19 @@ std::string Server::errorPage(int code) const {
 }
 
 void Server::bind() {
-
 	sockaddr_in servaddr = {};
 	servaddr.sin_family = AF_INET;
-	//servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_addr.s_addr = htonl(custom_inet_addr(_address));
 
-	try {
-		if (_port <= 0 || _port > 65535)
-			throw std::runtime_error("Invalid port number\n");
-		servaddr.sin_port = htons(_port);
-	}
-	catch (const RequestException& exception) {
-	}
+	if (_port <= 0 || _port > 65535)
+		throw std::runtime_error("Invalid port number on server " + _server_name);
+	servaddr.sin_port = htons(_port);
 
 	((_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-	  ? throw std::runtime_error("Socket creation failed\n")
-	  : Logger::debug("Socket successfully created");
+		? throw std::runtime_error("Socket creation failed\n")
+		: Logger::debug("Socket successfully created");
 
 	// set socket as reusable
-
 	int option = 1;
 	((setsockopt(_fd, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option)) == -1))
 		? throw std::runtime_error("Setting to reusable failed\n")
@@ -197,12 +188,8 @@ void Server::bind() {
 		: Logger::debug("Socket successfully binded");
 
 	// https://man7.org/linux/man-pages/man2/listen.2.html
-	try {
-		listen(_fd, 256);
-		Logger::info("Server listening on port " + Utils::toString(_port));
-	} catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
+	listen(_fd, 256);
+	Logger::info("Server listening on port " + Utils::toString(_port));
 
 	// setting socket as non blocking
 	(fcntl(_fd, F_SETFL, O_NONBLOCK) == -1)
