2654f9f67459c7e9771d35394e82c6a603aa6a44
tinitiuset <tinitiuset@gmail.com>
2024-03-20 18:04:33 +0100

    Add RequestList class

---
+++ requests/RequestList.hpp
@@ -0,0 +1,21 @@
+#ifndef REQUESTLIST_HPP
+#define REQUESTLIST_HPP
+
+#include <vector>
+
+class RequestList {
+
+public:
+	RequestList();
+	~RequestList();
+
+	void addRequest(Request* request);
+	void removeRequest(int fd);
+
+	Request* getRequest(int fd);
+
+private:
+	std::vector<Request*> _requests;
+};
+
+#endif
